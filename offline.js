9062888a9408bdd836a542b2958adc3c63d6ea6d
auron1na <emanuelmva12@gmail.com>
2024-10-14 19:00:40 -0600

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1728944613,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/fondo-sheet0.png",
+		"images/silu-sheet0.png",
+		"images/pe-sheet0.png",
+		"images/pie-sheet0.png",
+		"images/pac.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
